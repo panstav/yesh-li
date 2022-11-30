1bfe70a058bd81a23a3594c72d8f468310155ed0
Stav Geffen <stavgeffen@gmail.com>
2022-11-30 12:27:48 +0000

    fix(Guides) sets better minWidth to each guide

--- src/components/pages/RetreatYetziraVeOtzma/Guides/guides.js
+++ src/components/pages/RetreatYetziraVeOtzma/Guides/guides.js
@@ -1,10 +1,3 @@
-import Section from '@wrappers/Section';
-import GlassBox from '@wrappers/GlassBox';
-
-import LazyImage from '@elements/LazyImage';
-
-import Heading from './_elements/Heading';
-
 const guides = [
 	{
 		imageSrc: 'https://storage.googleapis.com/yeshli-www/samar-retreat-yetzira-ve-otzma/shila-01.jpg',
@@ -20,19 +13,4 @@ const guides = [
 	}
 ];
 
-export default function Guides () {
-	return <Section>
-		<GlassBox>
-			<Heading>על המנחה והמפיקה</Heading>
-			<div className="is-flex is-flex-direction-row is-flex-wrap-wrap is-flex-gap-4">
-				{guides.map((guide) => {
-					return <div className='is-flex-grow-1' style={{ flexBasis: 0, minWidth: '20rem' }}>
-						<LazyImage src={guide.imageSrc} className="is-round" style={{ width: '100px', borderRadius: '100%' }} />
-						<h3 className='mt-2 mb-1 has-text-weight-bold'>{guide.name}</h3>
-						<p>{guide.description}</p>
-					</div>
-				})}
-			</div>
-		</GlassBox>
-	</Section>;
-}
+export default guides;
---
+++ src/components/pages/RetreatYetziraVeOtzma/Guides/index.js
@@ -0,0 +1,25 @@
+import Section from '@wrappers/Section';
+import GlassBox from '@wrappers/GlassBox';
+
+import LazyImage from '@elements/LazyImage';
+
+import Heading from '../_elements/Heading';
+
+import guides from './guides';
+
+export default function Guides () {
+	return <Section>
+		<GlassBox>
+			<Heading>על המנחה והמפיקה</Heading>
+			<div className="is-flex is-flex-direction-row is-flex-wrap-wrap is-flex-gap-4">
+				{guides.map((guide) => {
+					return <div className='is-flex-grow-1' style={{ flexBasis: 0, minWidth: 'min(100%, 16rem)' }}>
+						<LazyImage src={guide.imageSrc} className="is-round" style={{ width: '100px', borderRadius: '100%' }} />
+						<h3 className='mt-2 mb-1 has-text-weight-bold'>{guide.name}</h3>
+						<p>{guide.description}</p>
+					</div>
+				})}
+			</div>
+		</GlassBox>
+	</Section>;
+}
