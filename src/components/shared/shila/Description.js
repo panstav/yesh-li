a2e7a9d365e3acbe376326288b2d51c9a29b5d10
Stav Geffen <stavgeffen@gmail.com>
2023-03-09 17:03:40 +0200

    fix(Description) sets a much nicer hero

--- src/components/shared/shila/Description.js
+++ src/components/shared/shila/Description.js
@@ -7,27 +7,29 @@ import { PageContext } from '@shared/shila/contexts';
 import Signup from '@shared/shila/Signup';
 
 export default function Description ({ children }) {
-	return <Section withTopMargin={false} className="mt-5 is-medium">
-		<GlassBox>
+	return <>
+		<Section withTopMargin={false} style={{ marginTop: '25vh', marginBottom: '25vh', textShadow: '0 0 10px white' }}>
 			<Title />
-			<div className='mt-5 content'>
-				{children}
-			</div>
-			<Signup className="mt-6" />
-		</GlassBox>
-	</Section>;
+		</Section>
+		<Section className="mt-5 is-medium">
+			<GlassBox>
+				<div className='mt-5 content'>
+					{children}
+				</div>
+				<Signup className="mt-6" />
+			</GlassBox>
+		</Section>
+	</>;
 }
 
 function Title() {
 	const { title, dates, isSoldOut } = useContext(PageContext);
 	return <h1 className="has-text-centered">
-		<span className="is-size-2 has-text-weight-bold">{title}</span>
-		<br />
+		<div className="is-size-2 has-text-weight-bold">{title}</div>
 		<span className="is-size-3 has-text-weight-bold has-text-grey">{dates}</span>
 		{isSoldOut && <>
 			<br />
-			<span className="is-size-5 has-text-weight-bold has-text-danger-dark">הסדנא מלאה</span>
-			<br />
+			<div className="is-size-4 has-text-weight-bold has-text-danger-dark mt-5">הסדנא מלאה</div>
 			<span className="is-size-6 has-text-weight-bold has-text-danger-dark">ניתן להשאיר פרטים לקבלת עידכונים על הסדנאות הבאות</span>
 		</>}
 	</h1>;
