b077fbd45e2254c852e75710b3cff5f93eeb60ab
Stav Geffen <stavgeffen@gmail.com>
2023-09-30 11:05:59 +0200

    fix(Editor) adds styles for mobile

--- src/components/pages/Editor/Editor.js
+++ src/components/pages/Editor/Editor.js
@@ -6,9 +6,11 @@ import Section from '@wrappers/Section';
 import Loader from '@elements/Loader';
 
 import { UserContext } from '.';
+import { fieldsContainer, previewContainer } from './index.module.sass';
 
 import ThemeFields from './ThemeFieldGroups';
 import Preview from './Preview';
+import classNames from 'classnames';
 
 export default function Editor () {
 
@@ -19,25 +21,27 @@ export default function Editor () {
 		defaultValues: () => xhr.getSiteData(siteId)
 	});
 
+	if (!Object.keys(form.getValues()).length) return <Loader />;
+
 	const submitForm = form.handleSubmit((data) => xhr.updateSiteData(siteId, data.content)
 		.then(() => alert('העמוד נשמר בהצלחה!'))
 		.catch(() => alert('אירעה שגיאה בעת שמירת העמוד. המידע נשמר אך העמוד לא התעדכן - צרו עימנו קשר בהקדם')));
 
-	if (!Object.keys(form.getValues()).length) return <Loader />;
+	const previewContainerClassName = classNames('p-3', previewContainer);
 
 	// get theme name from form values, capitalize first letter
 	const themeName = ((lowerCaseName) => (lowerCaseName[0].toUpperCase() + lowerCaseName.slice(1)))(form.getValues().theme);
 
 	return <FormProvider {...form}>
-		<div className='is-flex' style={{ height: '100%' }}>
-			<div style={{ minWidth: '25rem', width: 'min-content', height: '100%', overflowY: 'auto' }}>
-				<Section noTopMargin className='is-small py-3' style={{ minHeight: '10rem' }}>
+		<div className='is-flex-desktop' style={{ height: '100%' }}>
+			<div className={fieldsContainer}>
+				<Section noTopMargin className='py-3' style={{ minHeight: '10rem' }}>
 					<div className='has-strong-radius'>
 						<ThemeFields fieldGroupName={themeName} submitForm={submitForm} />
 					</div>
 				</Section>
 			</div>
-			<div className='p-3' style={{ width: '100%' }}>
+			<div className={previewContainerClassName}>
 				<Preview />
 				{/* <pre style={{ direction: 'ltr' }}>{JSON.stringify(form.getValues(), null, 2)}</pre> */}
 			</div>
