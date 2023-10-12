c37f499518a51009332197d1a9adfedd59907af9
Stav Geffen <stavgeffen@gmail.com>
2023-10-12 19:05:55 +0200

    fix(Alon) adds a maxWidth to media side

--- src/components/themes/Alon-01/Media/index.js
+++ src/components/themes/Alon-01/Media/index.js
@@ -6,7 +6,7 @@ export default function Media({ position, ...props }) {
 
 	const containerClassName = classNames(container, "is-relative");
 
-	return <div className={containerClassName} style={{ width: '100%' }}>
+	return <div className={containerClassName} style={{ width: '100%', maxWidth: '1400px' }}>
 		<img className="object-fit-cover" {...props} style={{ objectPosition: position }} />
 	</div>;
 }
