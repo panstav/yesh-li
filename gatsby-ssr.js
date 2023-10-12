d18fd84ff2fb24e668f8ed05676c946466117ee4
Stav Geffen <stavgeffen@gmail.com>
2023-10-12 13:40:05 +0200

    fix(gatsby) ensures preload and preconnect tags are as high in head as possible

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,3 +10,21 @@ lazySizes.cfg.expand = 300;
 export function onRenderBody({ setHtmlAttributes }) {
 	setHtmlAttributes({ lang: 'he', dir: 'rtl' });
 }
+
+export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+	const headComponents = getHeadComponents();
+
+	promote(headComponents, (item) => ['preload', 'preconnect'].includes(item?.props?.rel));
+
+	replaceHeadComponents(headComponents);
+};
+
+function promote(array, findFn) {
+	// find indexes of all items that match the findFn
+	const indexes = array.reduce((acc, item, index) => {
+		if (findFn(item)) acc.push(index);
+		return acc;
+	}, []);
+	// push those items to the top of the array
+	indexes.forEach((index) => array.unshift(array.splice(index, 1)[0]));
+}
