36344b8b6acfd84dfeee8aa9bed364cfa21c7bdd
Stav Geffen <stavgeffen@gmail.com>
2023-10-14 19:40:44 +0200

    fixup fix(useModal) supports going back to hideModal

--- src/components/wrappers/Modal/use-modal.js
+++ src/components/wrappers/Modal/use-modal.js
@@ -13,21 +13,32 @@ export default function useModal(propsFromHook = {}) {
 		return setModalProps();
 	};
 
+	const hideModalById = useCallback((event) => {
+		// the modal is being closed by the browser's back button
+		// ensure that the modal being closed is the one that was opened last
+		if (modalProps?.modalId && (event?.state?.modalId === modalProps?.modalId)) hideModal(event);
+	}, [modalProps, hideModal]);
+
 	useEffect(() => {
-		window.addEventListener('popstate', hideModal);
-		return () => window.removeEventListener('popstate', hideModal);
-	}, []);
+		if (!modalProps?.modalId) return;
+		window.addEventListener('popstate', hideModalById);
+		return () => window.removeEventListener('popstate', hideModalById);
+	}, [hideModalById, modalProps?.modalId]);
 
 	const showModal = useCallback((propsFromCallback = {}) => {
-		const newProps = Object.assign({ hideModal },
+		const modalId = Math.random().toString(36).slice(2);
+		const newProps = Object.assign({ hideModal, modalId },
 			typeof propsFromHook === 'function'
 				? propsFromHook(propsFromCallback)
 				: Object.assign(propsFromHook, propsFromCallback)
 		);
 		window.document.documentElement.classList.toggle('with-modal', true);
+		// this is to identify the modal so that if 2 are open and the user triggers hideModal - it wouldn't close both
+		history.replaceState({ modalId }, '');
 		history.pushState({}, '');
 		setModalProps(newProps);
 	}, [typeof propsFromHook]);
 
 	return [modalProps, showModal, hideModal];
+
 }
