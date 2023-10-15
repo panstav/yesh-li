8537049af0b284ce9d964649099c4c8a1e4da363
Stav Geffen <stavgeffen@gmail.com>
2023-10-15 14:54:06 +0200

    fix(Alon) featuredImage

--- src/themes/alon-01.js
+++ src/themes/alon-01.js
@@ -2,11 +2,12 @@ import { HeadFor } from "@config/Meta";
 import Page from "@themes/Alon-01";
 
 export const Head = HeadFor(({ pageContext: { content: { fullName, description, occupation, featuredImage } } }) => {
+	const featuredImageSrcs = featuredImage.srcSet.split(', ');
 	return {
-		_preload: [{ href: featuredImage.srcSet.split(' ')[0], as: 'image' }],
+		_preload: [{ href: featuredImageSrcs[0].split(' ')[0], as: 'image' }],
 		title: `${occupation} • ${fullName}`,
 		description,
-		featuredImage
+		featuredImage: featuredImageSrcs.slice(-1)[0].split(' ')[0]
 	};
 });
 
