0c13265258e96b5e3490d4fd4ab7dbbf4d2befbe
Stav Geffen <stavgeffen@gmail.com>
2023-10-16 12:18:44 +0200

    fix(details) extracts details.details to common css

--- src/components/pages/Editor/Fields/Details.js
+++ src/components/pages/Editor/Fields/Details.js
@@ -1,7 +1,10 @@
+import classNames from "classnames";
+
 import { details, innerContainer } from "@pages/Editor/index.module.sass";
 
 export default function Detail({ title, children }) {
-	return <details className={details}>
+	const detailsClassName = classNames(details, "details");
+	return <details className={detailsClassName}>
 		<summary>{title}</summary>
 		<div className={innerContainer}>
 			{children}
