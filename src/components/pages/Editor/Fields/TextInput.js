fa68a2e5bd73d72d68b1ad70ccf398ec08436eaf
Stav Geffen <stavgeffen@gmail.com>
2023-10-16 12:23:33 +0200

    fix(TextInput) defends

--- src/components/pages/Editor/Fields/TextInput.js
+++ src/components/pages/Editor/Fields/TextInput.js
@@ -12,16 +12,20 @@ export default function TextInput({ id, label, labelClassName: labelClasses, typ
 	const valueAs = (val) => setValueAs(cleanUGT(val));
 	const labelClassName = classNames('label', labelClasses);
 	const inputClassName = classNames('input', isSmall && 'is-small');
+
+	const inputConfig = {
+		required: required && copy.requiredField,
+		maxLength: maxLength && { value: maxLength, message: copy.maxLengthField(maxLength) },
+		setValueAs: valueAs
+	};
+
+	if (pattern) inputConfig.pattern = pattern;
+	if (validate) inputConfig.validate = validate;
+	if (onChange) inputConfig.onChange = (event) => onChange(ifValid({ validate, pattern }, valueAs(event.target.value)));
+
 	return <div className='field'>
 		<label htmlFor={id} className={labelClassName}>{label}:</label>
-		<input id={id} className={inputClassName} type={type} {...register(id, {
-			required: required && copy.requiredField,
-			pattern,
-			maxLength: maxLength && { value: maxLength, message: copy.maxLengthField(maxLength) },
-			validate,
-			onChange: (event) => onChange(ifValid({ validate, pattern }, valueAs(event.target.value))),
-			setValueAs: valueAs
-		})} />
+		<input id={id} className={inputClassName} type={type} {...register(id, inputConfig)} />
 		{error?.message
 			? <p className='help is-danger'>{error?.message}</p>
 			: <p className='help'>{description}</p>}
@@ -58,11 +62,11 @@ export function EmailInput(props) {
 
 function ifValid({ validate, pattern }, value) {
 
-	// if there's no value, don't validate
-	if (!value) return value;
+	// if there's no value or no validators - don't validate
+	if (!value || (!validate && !pattern)) return value;
 
 	// if there's no valid value, return false
-	return Object.values(validate).every(validator => validator(value))
+	return (!validate || Object.values(validate).every(validator => validator(value)))
 		&& (!pattern || (pattern instanceof RegExp ? pattern.test(value) : pattern.value.test(value)))
 		&& value;
 }
