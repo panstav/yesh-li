9c509fde3ad0bbb9a763270e669b89eee1ed6c25
Stav Geffen <stavgeffen@gmail.com>
2023-11-22 16:55:54 +0530

    Update createTrial function to include timeout option

--- src/services/xhr/index.js
+++ src/services/xhr/index.js
@@ -17,7 +17,7 @@ export default {
 	postImage: ({ imageBase64, fileName, sizes, siteSlug, isFavicon, position }) => post(`image`, { imageBase64, fileName, sizes, siteSlug, isFavicon, position }, { timeout: false }),
 	postYoutubeThumbnail: ({ videoUrl, siteId }) => post(`youtube-thumbnail`, { videoUrl, siteId }),
 	updateSiteData: (siteId, data) => put(`site-data?siteId=${siteId}`, data),
-	createTrial: (data) => post('trial', data),
+	createTrial: (data) => post('trial', data, { timeout: false }),
 	updateSlug: ({ slug, siteId }) => put('slug', { slug, siteId }),
 	checkSlugVacancy: (slug) => get(`slug-vacancy?slug=${slug}`),
 
