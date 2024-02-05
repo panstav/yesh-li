16256f92214cdcd240c2a3f928e480f7c1defc7e
Stav Geffen <stavgeffen@gmail.com>
2024-02-05 16:49:49 +0700

    fix(ImageInput) adds margin between entire image and crops

--- src/components/elements/Fields/ImageInput.js
+++ src/components/elements/Fields/ImageInput.js
@@ -127,7 +127,7 @@ export default function ImageInput({ id, label, description, sizes, multiple = f
 				<div className='mx-auto' style={{ width: '20rem' }}>
 					<img ref={ref} srcSet={imageToFocus.srcSet} />
 				</div>
-				<div className='is-flex is-justify-content-space-evenly is-align-items-center mt-5'>
+				<div className='is-flex is-justify-content-space-evenly is-align-items-center mt-6'>
 					<div className='mx-auto has-radius' style={{ width: '6rem', height: '10rem', overflow: 'hidden', border: '0.25em solid var(--color-primary)' }}>
 						<img srcSet={imageToFocus.srcSet} style={{ objectFit: 'cover', width: '100%', height: '100%', objectPosition: position }} />
 					</div>
