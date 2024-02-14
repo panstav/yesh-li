8ad25e1609cfc7e98f01d6e123f8d60698c2502c
Stav Geffen <stavgeffen@gmail.com>
2024-02-14 16:40:48 +0700

    fix(homepage) WhosItFor formatting

--- src/components/pages/Index/WhosItFor.js
+++ src/components/pages/Index/WhosItFor.js
@@ -7,13 +7,13 @@ export default function WhosItFor () {
 		<SectionTitle>למי זה הכי מתאים?</SectionTitle>
 		<div className="is-flex-desktop is-align-items-center is-flex-gap-5">
 			<div>
-				<p className="box is-shadowless has-background-primary has-strong-radius has-text-white is-size-5 mb-5-touch">יש.לי הכי מתאימה <b>לבעלי עסקים שמחפשים פיתרון איכותי לשדרוג הנוכחות ברשת</b>, מבלי ללמוד את הפרטים הטכניים של אתר אינטרנט, תהליך שלוקח שנים לאנשי המקצוע. במקום להשקיע בשבועות ולפעמים בחודשים של פיתוח אתר שמותאם אישית על ידי מקצוען, המערכת מציעה <b>פיתרון נוח, יעיל ומיידי שמאפשר לך להתקדם בפיתוח העסקי.</b></p>
+				<p className="box is-shadowless has-background-primary has-strong-radius has-text-white is-size-5 mb-5-touch">יש.לי הכי מתאימה <b>לבעלי עסקים שמחפשים פיתרון איכותי לשידרוג הנוכחות ברשת</b>, מבלי ללמוד את הפרטים הטכניים של אתר אינטרנט, תהליך שלוקח שנים לאנשי המקצוע. במקום להשקיע בשבועות ולפעמים בחודשים של פיתוח אתר שמותאם אישית על ידי מקצוען, המערכת מציעה <b>פיתרון נוח, יעיל ומיידי שמאפשר לך להתקדם בפיתוח העסקי.</b></p>
 			</div>
 			<div>
-				<p className="box is-shadowless has-background-danger-light has-strong-radius mb-5-touch"><b>המערכת פחות מתאימה למתכנתים/ות.</b> יש אפשרות להטמיע פתרונות תכנותיים מותאמים אישית, אך סביר שישתלם יותר לעבוד עם מערכת ייעודית כמו WordPress.</p>
+				<p className="box is-shadowless has-background-danger-light has-strong-radius mb-5-touch">המערכת פחות מתאימה <b>למתכנתים/ות</b>. יש אפשרות להטמיע פתרונות תכנותיים מותאמים אישית, אך סביר שישתלם יותר לעבוד עם מערכת ייעודית כמו WordPress.</p>
 			</div>
 			<div>
-				<p className="box is-shadowless has-background-danger-light has-strong-radius mb-5-touch"><b>המערכת פחות מתאימה למעצבים/ות.</b> יש אפשרות להטמיע פתרונות עיצוביים מותאמים אישית, אך סביר שישתלם יותר לעבוד עם מערכת ייעודית כמו Wix.</p>
+				<p className="box is-shadowless has-background-danger-light has-strong-radius mb-5-touch">המערכת פחות מתאימה <b>למעצבים/ות</b>. יש אפשרות להטמיע פתרונות עיצוביים מותאמים אישית, אך סביר שישתלם יותר לעבוד עם מערכת ייעודית כמו Wix.</p>
 			</div>
 		</div>
 	</Section>;
