e755b09ec8f82b9448a5a62e1ee75c94894a5806
Stav Geffen <stavgeffen@gmail.com>
2024-02-21 15:33:00 +0700

    feat(ContactParagraph)

--- src/components/pages/PrivacyPolicy.js
+++ src/components/pages/PrivacyPolicy.js
@@ -1,3 +1,4 @@
+import ContactParagraph from "@elements/ContactParagraph";
 import ContentPage from "@elements/ContentPage";
 import LastUpdated from "@elements/LastUpdated";
 
@@ -65,11 +66,7 @@ export default function PrivacyPolicy() {
 
 		<p>במקרה של ביטול עסקה, ובהתאם לחוק הגנת הצרכן, המשתמש יהיה זכאי להחזר כספי - זאת במידה ולא עברו יותר מ-14 יום מתחילת השירות ובהפחתה של ₪50 מסכום העסקה - זאת לטובה כיסוי עלויות התקנה ועמלת החיוב וההחזר של חברת האשראי.</p>
 
-		<h2>יצירת קשר</h2>
-
-		<p><b>יש.לי</b> היא מערכת מבית <b>סתו גפן - בניית אתרים</b><br />ח.פ 311655773<br />וולפסון 49, ראשון לציון</p>
-
-		<p>לכל שאלה או טענה - אנא שלחו לנו מייל לכתובת <a href="mailto:hello@yesh.li" target="_blank" rel="noopener noreferrer">hello@yesh.li</a>.</p>
+		<ContactParagraph />
 
 	</ContentPage>;
 }
