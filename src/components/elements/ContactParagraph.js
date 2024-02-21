68bae5fa34cbfe6183a3cd52860cadbbffe90c68
Stav Geffen <stavgeffen@gmail.com>
2024-02-21 16:46:55 +0700

    fix(ContactParagraph) removes address

--- src/components/elements/ContactParagraph.js
+++ src/components/elements/ContactParagraph.js
@@ -1,9 +1,12 @@
+const address = '';
+// const address = 'וולפסון 49, ';
+
 export default function ContactParagraph() {
 	return <>
 
 		<h2>יצירת קשר</h2>
 
-		<p><b>יש.לי</b> היא מערכת מבית <b>סתו גפן - בניית אתרים</b><br />ח.פ 311655773<br />וולפסון 49, ראשון לציון</p>
+		<p><b>יש.לי</b> היא מערכת מבית <b>סתו גפן - בניית אתרים</b><br />ח.פ 311655773<br />{address}ראשון לציון</p>
 
 		<p>לכל שאלה או טענה - אנא שלחו לנו מייל לכתובת <a href="mailto:hello@yesh.li" target="_blank" rel="noopener noreferrer">hello@yesh.li</a>.</p>
 
