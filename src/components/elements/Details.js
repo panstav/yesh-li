09537d4724f06cf91a5c36f94ad475cff0767ddc
Stav Geffen <stavgeffen@gmail.com>
2024-03-09 18:33:54 +0700

    ocd

--- src/components/elements/Details.js
+++ src/components/elements/Details.js
@@ -2,7 +2,7 @@ import classNames from "classnames";
 
 import { details, innerContainer } from "@pages/Editor/index.module.sass";
 
-export default function Detail({ title, children }) {
+export default function Details ({ title, children }) {
 	const detailsClassName = classNames(details, "details");
 	return <details className={detailsClassName}>
 		<summary>{title}</summary>
