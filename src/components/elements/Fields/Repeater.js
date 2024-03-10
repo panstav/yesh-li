47fe1809d45ebad84414dce41b01c673c0a9c04c
Stav Geffen <stavgeffen@gmail.com>
2024-03-10 12:20:22 +0700

    feat(Editor) Repeater order editting

--- src/components/elements/Fields/Repeater.js
+++ src/components/elements/Fields/Repeater.js
@@ -1,12 +1,13 @@
 import { useFormContext, useFieldArray } from "react-hook-form";
 import classNames from "classnames";
 
-import { compoundField, removeButton, addButton } from "@pages/Editor/index.module.sass";
+import { compoundField, repeatedButton, addButton } from "@pages/Editor/index.module.sass";
+import { ArrowDown, ArrowUp, Close } from "@elements/Icon";
 
 export default function Repeater({ arrayId, singleName, emptyItem, minLength, maxLength, children }) {
 
 	const { getValues } = useFormContext();
-	const { fields, append, remove } = useFieldArray({ name: arrayId });
+	const { fields, append, remove, move } = useFieldArray({ name: arrayId });
 
 	const addToBottom = () => {
 		if (typeof emptyItem !== 'function') return append(emptyItem);
@@ -16,16 +17,41 @@ export default function Repeater({ arrayId, singleName, emptyItem, minLength, ma
 	const cantRemove = (minLength && fields.length == minLength) ? `מינימום ${minLength} פריטים` : '';
 	const cantAdd = maxLength && fields.length == maxLength ? `מקסימום ${maxLength} פריטים` : '';
 
-	const removeButtonClassName = classNames(removeButton, 'button is-small has-text-weight-bold');
+	const repeatedButtonClassName = classNames(repeatedButton, 'button is-small has-text-weight-bold');
 	const addButtonClassName = classNames(addButton, 'button is-fullwidth has-text-weight-bold');
 
 	return <>
 		{fields.map((field, index) => {
+
 			return <div className={compoundField} key={field.id}>
 
 				<div className="is-flex is-justify-content-space-between is-align-items-center mb-4">
 					<h3 className="is-size-5 m-0">{singleName} #{index + 1}</h3>
-					<button type="button" onClick={() => remove(index)} data-index={index} className={removeButtonClassName} disabled={!!cantRemove} title={cantRemove}>להסיר</button>
+					<div className="buttons has-addons">
+
+						{index !== 0 && <button
+							type="button" className={repeatedButtonClassName}
+							onClick={() => move(index, index - 1)}
+							title="לדחוף מעלה" data-index={index}
+						>
+							<ArrowUp style={{ width: '0.75rem' }} />
+						</button>}
+
+						{index !== fields.length - 1 && <button
+							type="button" className={repeatedButtonClassName}
+							onClick={() => move(index, index + 1)}
+							title="לדחוף מטה"
+						>
+							<ArrowDown style={{ width: '0.75rem' }} />
+						</button>}
+						<button
+							type="button" className={repeatedButtonClassName}
+							disabled={!!cantRemove} onClick={() => remove(index)}
+							title={cantRemove || 'להסיר'} data-index={index}
+						>
+							<Close />
+						</button>
+					</div>
 				</div>
 
 				{children(`${arrayId}.[${index}]`)}
