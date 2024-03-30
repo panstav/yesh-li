a7bb2ad909ec67f64073b9f27c577c04fdabf8da
Stav Geffen <stavgeffen@gmail.com>
2024-03-30 10:51:45 +0545

    fix(cleanUGT) removes any html attributes that may accompany a paste

--- src/lib/clean-user-generated-text.js
+++ src/lib/clean-user-generated-text.js
@@ -1,3 +1,13 @@
 export default function cleanUGT (str) {
-	return str.replace(/\s+\./g, '. ').replace(/\s+,/g, ', ').replace(/\s+/g, ' ').trim();
+	return str
+		// ensure that dots are followed by a space instead of the other way around
+		.replace(/\s+\./g, '. ')
+		// ensure that commas are followed by a space instead of the other way around
+		.replace(/\s+,/g, ', ')
+		// ensure that a space is not followed by another space
+		.replace(/\s+/g, ' ')
+		// Remove all attributes from any HTML tags
+		.replace(/\s*\S*="[^"]+"\s*/gm, "")
+		// Remove spaces from the beginning and end of the string
+		.trim();
 }
