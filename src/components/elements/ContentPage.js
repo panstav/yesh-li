d198236a0de99d19338af89ee07acc9bcbe55868
Stav Geffen <stavgeffen@gmail.com>
2024-04-09 15:48:50 +0545

    fix(ContentPage) utilizes SafeHeader instead of redoing a Nav

--- src/components/elements/ContentPage.js
+++ src/components/elements/ContentPage.js
@@ -1,15 +1,9 @@
-import { Link } from "gatsby";
-
-import { Logo } from "@elements/Icon";
 import Section from "@wrappers/Section";
+import { SafeHeader } from "@pages/Editor/Header";
 
 export default function ContentPage({ children }) {
 	return <>
-		<div className="has-background-primary has-text-centered pt-3 pb-1">
-			<Link to="/">
-				<Logo className="has-text-white" style={{ width: '4rem' }} />
-			</Link>
-		</div>
+		<SafeHeader />
 		<Section>
 			<div className="box content mb-6">
 				{children}
