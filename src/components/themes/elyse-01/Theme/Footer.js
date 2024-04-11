8f385b22fd38b14c09aba43f3310dc4a38c64dbf
Stav Geffen <stavgeffen@gmail.com>
2024-04-11 16:52:08 +0545

    fixup feat(mutli & i18n)

--- src/components/themes/elyse-01/Theme/Footer.js
+++ src/components/themes/elyse-01/Theme/Footer.js
@@ -1,11 +1,12 @@
+import { useContext } from 'react';
+
 import Section from '@wrappers/Section';
 
-import usePageData from '@hooks/use-page-data';
-
+import { PageContext } from '.';
 import Socials from './Socials';
 
 export default function Footer () {
-	const { css, content: { fullName, socials } } = usePageData();
+	const { css, content: { fullName, socials } } = useContext(PageContext);
 
 	return <footer className="footer has-text-centered has-background-white pb-5" style={{ borderTop: css.border }}>
 
