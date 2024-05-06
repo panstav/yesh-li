7a54e6167bfed45bb3c96deb27d4c3928b2f3524
Stav Geffen <stavgeffen@gmail.com>
2024-05-06 15:00:58 +0545

    feat(Repeater) adds collapsable items with Details

--- src/components/elements/Fields/Repeater.js
+++ src/components/elements/Fields/Repeater.js
@@ -3,15 +3,19 @@ import classNames from "classnames";
 
 import { useFieldLabels } from "@hooks/use-i18n";
 
-import { compoundField, repeatedButton, addButton } from "@pages/Editor/index.module.sass";
+import RenderChildren from "@wrappers/RenderChildren";
 import { ArrowDown, ArrowUp, Close } from "@elements/Icon";
 
-export default function Repeater({ arrayId, singleName, emptyItem, minLength, maxLength, children }) {
+import { compoundField, repeatedButton, addButton } from "@pages/Editor/index.module.sass";
+
+export default function Repeater({ arrayId, singleName, titleFieldId, emptyItem, hideItemTitle, minLength, maxLength, wrapper: Wrapper = RenderChildren, children }) {
 
 	if (!arrayId) throw new Error('Repeater: arrayId is required');
 	if (!singleName) throw new Error(`Repeater (${arrayId}): singleName is required`);
 	if (!emptyItem) throw new Error(`Repeater (${arrayId}): emptyItem is required`);
 	if (!children || typeof children !== 'function') throw new Error(`Repeater: children is required and must (${arrayId}) be a function`);
+
+	if (!Wrapper) Wrapper = collapseItems ? CollapsedRepeaterItem : RenderChildren;
 
 	const t = useFieldLabels();
 	const { getValues } = useFormContext();
@@ -31,11 +35,18 @@ export default function Repeater({ arrayId, singleName, emptyItem, minLength, ma
 	return <>
 		{fields.map((field, index) => {
 
-			return <div className={compoundField} key={field.id}>
+			const itemId = `${arrayId}.[${index}]`;
 
-				<div className="is-flex is-justify-content-space-between is-align-items-center mb-4">
-					<h3 className="is-size-5 m-0">{singleName} #{index + 1}</h3>
-					<div className="buttons has-addons">
+			let itemTitle;
+			// prefer fieldTitle if titleId is given and is not empty
+			if (!hideItemTitle) itemTitle = (titleFieldId ? getValues(`${arrayId}.[${index}].${titleFieldId}`) : '') || `${singleName} #${index + 1}`;
+
+			return <Wrapper itemId={itemId} key={field.id}>
+				<div className={compoundField}>
+
+					<div className="is-flex is-justify-content-space-between is-align-items-center mb-4">
+						{hideItemTitle ? <span /> : <h3 className="is-size-5 m-0">{itemTitle}</h3>}
+						<div className="buttons has-addons">
 
 						{index !== 0 && <button
 							type="button" className={repeatedButtonClassName}
@@ -60,6 +71,8 @@ export default function Repeater({ arrayId, singleName, emptyItem, minLength, ma
 							<Close />
 						</button>
 					</div>
+
+					{children(itemId)}
 				</div>
 
 				{children(`${arrayId}.[${index}]`)}
@@ -68,4 +81,10 @@ export default function Repeater({ arrayId, singleName, emptyItem, minLength, ma
 		<button type="button" onClick={addToBottom} className={addButtonClassName} disabled={!!cantAdd} title={cantAdd}>{t.addItem(singleName)}</button>
 	</>;
 
+}
+
+function CollapsedRepeaterItem ({ title, children, ...props }) {
+	return <Details title={title} {...props}>
+		{children}
+	</Details>;
 }
