7d469fafdd555bdacbf238b9f6dba164d914fe67
Stav Geffen <stavgeffen@gmail.com>
2024-05-07 22:07:14 +0545

    feat(Repeater) supports Modal items

--- src/components/elements/Fields/Repeater.js
+++ src/components/elements/Fields/Repeater.js
@@ -1,15 +1,15 @@
 import { useFormContext, useFieldArray } from "react-hook-form";
 import classNames from "classnames";
 
-import { useFieldLabels } from "@hooks/use-i18n";
+import useI18n, { useFieldLabels } from "@hooks/use-i18n";
 
 import Details from "@elements/Details";
+import Modal, { SaveButton, useModal } from "@wrappers/Modal";
 import { ArrowDown, ArrowUp, Close } from "@elements/Icon";
-import RenderChildren from "@wrappers/RenderChildren";
 
-import { compoundField, repeatedButton, addButton } from "@pages/Editor/index.module.sass";
+import { fieldsContainer, compoundField, repeatedButton, addButton, repeaterItemTitle } from "@pages/Editor/index.module.sass";
 
-export default function Repeater({ arrayId, singleName, emptyItem, collapseItems, addButtonOnTop, minLength, maxLength, wrapper: Wrapper, children }) {
+export default function Repeater({ arrayId, singleName, emptyItem, collapseItems, openItemInModal, addButtonOnTop, minLength, maxLength, wrapper: Wrapper, children }) {
 
 	if (!arrayId) throw new Error('Repeater: arrayId is required');
 	if (!singleName) throw new Error(`Repeater (${arrayId}): singleName is required`);
@@ -17,7 +17,9 @@ export default function Repeater({ arrayId, singleName, emptyItem, collapseItems
 	if (collapseItems && !emptyItem[collapseItems]) throw new Error(`Repeater (${arrayId}): emptyItem must have a non-falsy property named ${collapseItems}`);
 	if (!children || typeof children !== 'function') throw new Error(`Repeater: children is required and must (${arrayId}) be a function`);
 
-	if (!Wrapper) Wrapper = collapseItems ? CollapsedRepeaterItem : RenderChildren;
+	if (!Wrapper) Wrapper = collapseItems
+		? openItemInModal ? ModalizedRepeaterItem : CollapsedRepeaterItem
+		: NoWrapper;
 
 	const t = useFieldLabels();
 	const { getValues } = useFormContext();
@@ -38,48 +40,46 @@ export default function Repeater({ arrayId, singleName, emptyItem, collapseItems
 		{addButtonOnTop && <button type="button" onClick={addToBottom} className={addButtonClassName} disabled={!!cantAdd} title={cantAdd}>{t.addItem(singleName)}</button>}
 		{fields.map((field, index) => {
 
+			const itemId = `${arrayId}.[${index}]`;
 			let itemTitle = `${singleName} #${index + 1}`;
 			// prefer fieldTitle if titleId is given and is not empty
 			if (collapseItems) itemTitle = getValues(`${arrayId}.[${index}].${collapseItems}`);
 
 			const fieldHeaderClassName = classNames('is-flex is-align-items-center ', collapseItems ? 'is-justify-content-end' : 'is-justify-content-space-between mb-4');
 
-			return <Wrapper title={itemTitle} key={field.id}>
-				<div className={compoundField}>
+			return <Wrapper title={itemTitle} itemId={itemId} key={field.id}>
+				<div className={fieldHeaderClassName}>
+					{!collapseItems && <h3 className="is-size-5 m-0">{itemTitle}</h3>}
+					<div className="buttons has-addons">
 
-					<div className={fieldHeaderClassName}>
-						{!collapseItems && <h3 className="is-size-5 m-0">{itemTitle}</h3>}
-						<div className="buttons has-addons">
+						{index !== 0 && <button
+							type="button" className={repeatedButtonClassName}
+							onClick={() => move(index, index - 1)}
+							title={t.move_up} data-index={index}
+						>
+							<ArrowUp style={{ width: '0.75rem' }} />
+						</button>}
 
-							{index !== 0 && <button
-								type="button" className={repeatedButtonClassName}
-								onClick={() => move(index, index - 1)}
-								title={t.move_up} data-index={index}
-							>
-								<ArrowUp style={{ width: '0.75rem' }} />
-							</button>}
+						{index !== fields.length - 1 && <button
+							type="button" className={repeatedButtonClassName}
+							onClick={() => move(index, index + 1)}
+							title={t.move_down}
+						>
+							<ArrowDown style={{ width: '0.75rem' }} />
+						</button>}
 
-							{index !== fields.length - 1 && <button
-								type="button" className={repeatedButtonClassName}
-								onClick={() => move(index, index + 1)}
-								title={t.move_down}
-							>
-								<ArrowDown style={{ width: '0.75rem' }} />
-							</button>}
+						<button
+							type="button" className={repeatedButtonClassName}
+							disabled={!!cantRemove} onClick={() => remove(index)}
+							title={cantRemove || t.remove} data-index={index}
+						>
+							<Close />
+						</button>
 
-							<button
-								type="button" className={repeatedButtonClassName}
-								disabled={!!cantRemove} onClick={() => remove(index)}
-								title={cantRemove || t.remove} data-index={index}
-							>
-								<Close />
-							</button>
-
-						</div>
 					</div>
-
-					{children(`${arrayId}.[${index}]`)}
 				</div>
+
+				{children(itemId)}
 			</Wrapper>;
 		})}
 		{!addButtonOnTop && <button type="button" onClick={addToBottom} className={addButtonClassName} disabled={!!cantAdd} title={cantAdd}>{t.addItem(singleName)}</button>}
@@ -87,8 +87,45 @@ export default function Repeater({ arrayId, singleName, emptyItem, collapseItems
 
 }
 
-function CollapsedRepeaterItem ({ title, children, ...props }) {
-	return <Details title={title} {...props}>
+function ModalizedRepeaterItem({ title, children }) {
+	const [{ misc: t }] = useI18n();
+
+	const [repeaterItemModal, showModal] = useModal();
+
+	const Title = typeof title === "function" ? title : () => <>{title}</>;
+
+	const titleWrapperClassName = classNames(repeaterItemTitle, 'is-clickable');
+
+	return <>
+		<TitleWithButtons
+			title={title}
+			onClick={handleShowingModal}
+			className={titleWrapperClassName} />
+
+		<Modal {...repeaterItemModal} noCloseButton render={() => <div className={fieldsContainer}>
+			{children}
+			<SaveButton>{t.continue}</SaveButton>
+		</div>} />
+	</>;
+}
+
+function CollapsedRepeaterItem({ title, children, ...props }) {
+	const Title = () => <TitleWithButtons title={title} titleProps={{ ['data-onclick-avoid-closing-details']: true }} />;
+	return <Details title={Title} {...props}>
 		{children}
 	</Details>;
+}
+
+function TitleWithButtons({ title, className: classes, onClick, titleProps }) {
+	const className = classNames("is-flex is-justify-content-space-between is-align-items-center pe-5", classes);
+	return <div className={className} onClick={onClick}>
+		<h3 className="is-size-5 m-0 has-text-wrap-ellipsis" {...titleProps}>{title}</h3>
+		<Buttons onlyOnHover />
+	</div>;
+}
+
+function NoWrapper ({ children }) {
+	return <div className={compoundField}>
+		{children}
+	</div>;
 }
