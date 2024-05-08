3d731fa5492c9ba0f784a30b4da151bdd1bb1667
Stav Geffen <stavgeffen@gmail.com>
2024-05-08 14:39:20 +0545

    fix(tom-01) sets background customComponent

--- src/components/themes/tom-01/Theme/config/Background.js
+++ src/components/themes/tom-01/Theme/config/Background.js
@@ -5,6 +5,7 @@ export default function Background() {
 		top: '-50%',
 		left: '-50%',
 		width: '200%',
-		height: '200%'
+		height: '200%',
+		backgroundImage: 'url(https://tombezrukov.com/static/background-01-ee7e68e94b7be12fbc044ec0ef34ce8f.jpg)',
 	}} />;
 }
