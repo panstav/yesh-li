8ae9646333246b5e36a432f0ac87af10dffa109e
Stav Geffen <stavgeffen@gmail.com>
2024-05-29 19:30:48 +0545

    test(production) adds manifest.json tests

--- test/production-test.js
+++ test/production-test.js
@@ -1,9 +1,44 @@
 import cp from 'child_process';
+import exp from 'constants';
 
 const controller = new AbortController();
 const { signal } = controller;
 
+let error;
+
+beforeAll(async () => {
+	error = await build();
+}, 500000);
+
 it('should run production build with no errors', async () => {
+	expect(error).toBeUndefined();
+});
+
+it('should correctly produce a manifest file', async () => {
+	const manifest = require('../static/manifest.json');
+
+	[
+		"id",
+		"name",
+		"short_name",
+		"theme_color",
+		"background_color"
+	].forEach((key) => {
+		expect(manifest[key]).toBeTruthy();
+	});
+
+	expect(manifest.display).toBe('standalone');
+	expect(manifest.orientation).toBe('portrait');
+	expect(manifest.start_url).toBe('/');
+
+	expect(manifest.icons).toHaveLength(2);
+	manifest.icons.forEach((icon) => {
+		expect(icon.src.includes('undefined')).not.toBeTruthy();
+	});
+
+});
+
+async function build() {
 
 	let error;
 	try {
@@ -32,6 +67,5 @@ it('should run production build with no errors', async () => {
 	}
 	controller.abort();
 
-	expect(error).toBeUndefined();
-
-}, 500000);
+	return error;
+}
