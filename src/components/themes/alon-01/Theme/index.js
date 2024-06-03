73f0f6dab8cbe056018c2849b8e765c9512769d1
Stav Geffen <stavgeffen@gmail.com>
2024-06-04 00:36:44 +0545

    fix(alon-01) adds page head title

--- src/components/themes/alon-01/Theme/index.js
+++ src/components/themes/alon-01/Theme/index.js
@@ -4,10 +4,12 @@ import parseSrcSet from "@lib/parse-srcset";
 
 export { default } from "./Theme";
 
-export const Head = HeadFor(({ pageContext: { content: { description, featuredImage } } }) => {
+export const Head = HeadFor(({ pageContext: { title, content: { description, featuredImage } } }) => {
 	const srcs = parseSrcSet(featuredImage.srcSet);
 	return {
 		preload: [{ href: srcs[0], as: 'image' }, { href: srcs[1], as: 'image' }],
+
+		title,
 		description,
 		featuredImage: srcs[srcs.length - 1]
 	};
