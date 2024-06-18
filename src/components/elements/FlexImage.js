e03f68a81b1bfe855ce6f5a25f72c06474dc5730
Stav Geffen <stavgeffen@gmail.com>
2024-06-18 18:47:53 +0545

    feat(FlexImage) supports wrapper

--- src/components/elements/FlexImage.js
+++ src/components/elements/FlexImage.js
@@ -1,7 +1,15 @@
 import classNames from "classnames";
+import RenderChildren from "@wrappers/RenderChildren";
 
-export default function FlexImage({ position = '50% 50%', className: classes, ...props }) {
+export default function FlexImage({ position = '50% 50%', className: classes, wrapper, wrapperProps = {}, ...props }) {
+
 	const className = classNames('object-fit-cover', classes);
 	props.style = Object.assign({ objectPosition: position }, props.style || {});
-	return <img className={className} {...props} />;
+
+	const Wrapper = wrapper || RenderChildren;
+	const wrapperClasses = classNames('object-fit-cover', wrapperProps.className);
+
+	return <Wrapper {...wrapperProps} className={wrapperClasses}>
+		<img className={className} {...props} />
+	</Wrapper>;
 }
